1bae5e515d2dad4d74c4f665ac7e580bea60fb80
Ruby <graceffiong@gmail.com>
2022-11-13 00:29:06 +0100

    try

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,84 @@ import SignIn from './components/SignIn/SignIn'
 import SignUp from './components/SignUp/SignUp'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
+import React, { useEffect } from 'react'
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 function App() {
+  const clientId = '20911244888-9d2teo13j1j7qb3bhttpghvibg2gs92b.apps.googleusercontent.com';
+  const key = 'AIzaSyBVCUN3tMfZnl5iZQjGN31EkwVsaub4RV4';
+
+  useEffect(() => {
+    const initClient = () => {
+          gapi.auth2.init({
+          clientId: clientId,
+          apiKey: key,
+          scope: '',
+        });
+     };
+     gapi.load('client:auth2', initClient);
+ });
+
+  const onSuccess = (res) => {
+    console.log('success:', res);
+};
+const onFailure = (err) => {
+    console.log('failed:', err);
+};
+  // const onSuccess = (res) => {
+  //   setProfile(res.profileObj);
+  // };
+
+  // const onFailure = (err) => {
+  //   console.log('failed', err);
+  // };
+
+  // const logOut = () => {
+  //   setProfile(false);
+  // };
 
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<SignUp />} path='/sign-up' />
-        <Route element={<SignIn />} path='/sign-in' />
-        <Route element={<Dashboard />} path='/dashboard' />
-      </Routes>
-    </Router>
+    <div>
+      <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+
+      {/* {profile ? (
+                <div>
+                    <img src={profile.imageUrl} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={false}
+                />
+            )} */}
+    </div>
+    // <Router>
+    //   <Nav />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route element={<SignUp />} path='/sign-up' />
+    //     <Route element={<SignIn />} path='/sign-in' />
+    //     <Route element={<Dashboard />} path='/dashboard' />
+    //   </Routes>
+    // </Router>
   )
 }
 
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+// import { GoogleLogin, GoogleLogout } from 'react-google-login';
+// import { gapi } from 'gapi-script';
 
 const SignIn = () => {
+  // const [ profile, setProfile ] = useState(false);
+  // const clientId = "20911244888-9d2teo13j1j7qb3bhttpghvibg2gs92b.apps.googleusercontent.com"
+
+//   useEffect(() => {
+//     const initClient = () => {
+//           gapi.client.init({
+//           clientId: clientId,
+//           scope: ''
+//         });
+//      };
+//      gapi.load('client:auth2', initClient);
+//  });
+
+  // const onSuccess = (res) => {
+  //   setProfile(res.profileObj);
+  // };
+
+  // const onFailure = (err) => {
+  //   console.log('failed', err);
+  // };
+
+  // const logOut = () => {
+  //   setProfile(null);
+  // };
+
   return (
     <div>
       <h1>Sign In</h1>
       <button>Sign in with Google</button>
+      {/* {profile ? (
+                <div>
+                    <img src={profile.imageUrl} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={false}
+                />
+            )} */}
     </div>
   )
 }
